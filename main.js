00897d4b40069765f3eea40d68d6515c300054d2
Rafal Komar <rafalkomar@icloud.com>
2019-06-15 15:11:15 +0200

    refactor

--- main.js
+++ main.js
@@ -3,17 +3,14 @@ const name ='Rafal Komar';
 const age=26;
 console.log(name);
 console.log(age);
-
-
 console.log(`Nazywam sie ${name} i mam ${age} lat`)
 
 
 const heading = document.querySelector('.diary__paragraph--js');
-
 console.log(heading.innerHTML);
-
 heading.innerHTML=`Nazywam sie ${name} i mam ${age} lat`;
 
-const headingg = document.querySelectorAll('p');
 
-console.log(headingg[4]);
+
+const headingg = document.querySelectorAll('p');
+console.log(headingg[0]);
