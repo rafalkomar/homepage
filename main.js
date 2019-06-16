3078d667ad4f006852834ab523da2b44576627ef
Rafal Komar <rafalkomar@icloud.com>
2019-06-16 14:26:03 +0200

    done

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ console.log(`Nazywam sie ${name} i mam ${age} lat`)
 
 const heading = document.querySelector('.diary__paragraph--js');
 console.log(heading.innerHTML);
+
 heading.innerHTML=`Nazywam sie ${name} i mam ${age} lat`;
 
 
 
 const headingg = document.querySelectorAll('p');
 console.log(headingg[0]);
+
