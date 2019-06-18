763d2a0177a3eea31140a1e3f2a6827cddc319f0
Rafal Komar <rafalkomar@icloud.com>
2019-06-18 22:33:03 +0200

    correct css

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 console.log('siema');
 const name ='Rafal Komar';
-const age=26;
+const age=10;
 console.log(name);
 console.log(age);
 console.log(`Nazywam sie ${name} i mam ${age} lat`)
 
 
-const heading = document.querySelector('.diary__paragraph--js');
-console.log(heading.innerHTML);
 
-heading.innerHTML=`Nazywam sie ${name} i mam ${age} lat`;
 
 
 
@@ -18,3 +15,52 @@ console.log(headingg[0]);
 
 
 
+if ('javascript'!='java'){
+
+    console.log('prawda');
+}
+
+if (name || dupa){
+
+    console.log('brawo')
+}
+
+if(age<20){
+
+    console.log('masz wiecej niz 20 lat');
+} else if (age>=20 && age <= 30){
+    console.log('masz wiecej niz 20 lat ale mniej niz 30')
+}
+
+switch (age){
+
+    case 20:
+    console.log('masz rowno  20 lat');
+    break;
+
+    case 30:
+    console.log('masz rowno 30 lat');
+    break;
+
+    default:
+    console.log(`masz ${age} lat`);
+    break;        
+}
+
+
+
+
+const old= (age>10) ? 'yes': 'no';
+console.log(old);
+
+if (age<10) {
+
+    console.log('yes');
+    
+}else if (age>10) {
+    
+
+    }
+
+
+    
